b1400f9a39ad1fd1bda983494dd90ff1998396a2
raim-io <raim.tobi@gmail.com>
2024-01-17 02:48:27 +0100

    6-print_comb3.py: created

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""prints numbers from 0 to 99"""
+
+for i in range(9):
+    for j in range(i + 1, 10):
+        if i != 8:
+            print("{:02d}, ".format(i * 10 + j), end="")
+        else:
+            print("{:02d}".format(i * 10 +j))
