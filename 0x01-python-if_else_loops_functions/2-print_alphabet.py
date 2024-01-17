b7a442631860d663756669c2d8619cd57c84ce07
raim-io <raim.tobi@gmail.com>
2024-01-17 01:50:27 +0100

    2-print_alphabet.py: updated

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,4 +2,4 @@
 """print the ASCII alphabet in lower case, not followed  by a new line"""
 
 for ascii_letter in range(92, 123):
- print("{}".format(chr(ascii_letter)), end="")
+    print("{}".format(chr(ascii_letter)), end="")
