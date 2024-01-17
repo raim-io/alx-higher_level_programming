c76797e02920fcaa8e5131ecbf0b3a6437813741
root <raim.tobi@gmail.com>
2024-01-16 17:26:26 -0800

    4-print_hexa.py: updated

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """prints num 0 - 98 in hexadecimal"""
 
-for number in range(0,99):
-	print("{} = {}".format(number, hex(number)))
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
