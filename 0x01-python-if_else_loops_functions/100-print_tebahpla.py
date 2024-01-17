31e4322fbfa4b36563c8fb9a269f0c1688ba56a6
raim-io <raim.tobi@gmail.com>
2024-01-17 05:08:15 +0100

    100-print_tebahpla.py: updated

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 for i in range(ord('z'), ord('a') - 1, -1):
-	if i % 2 == 0:
-		case = 0
-	else:
-		case = 32
-	print("{}".format(chr(i - case)), end="")
+    if i % 2 == 0:
+        case = 0
+    else:
+        case = 32
+    print("{}".format(chr(i - case)), end="")
