21727c0d1dc4bd75da07450700517f33837d6afb
raim-io <raim.tobi@gmail.com>
2024-01-18 22:19:47 +0100

    3-infinite_add.py: updated

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-	import sys
+    import sys
 
-	sum = 0
-	for i in range(len(sys.argv) - 1):
-		sum += int(sys.argv[i + 1])
+    sum = 0
+    for i in range(len(sys.argv) - 1):
+        sum += int(sys.argv[i + 1])
 
-	print(sum)
+    print(sum)
