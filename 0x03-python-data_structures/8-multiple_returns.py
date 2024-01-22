b638e6665ca74ae5f30272dfd3766d2693832a90
raim-io <raim.tobi@gmail.com>
2024-01-23 00:07:39 +0100

    8-multiple_returns.py: updated

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
-#!.usr/bin/python3
+#!usr/bin/python3
 
 def multiple_returns(sentence):
-    my_tuple = ()
+    _tuple = ()
     if len(sentence) == 0:
-        my_tuple = 0, "None"
+        _tuple = 0, "None"
     else:
-        my_tuple = len(sentence), sentence[0]
-    return my_tuple
+        _tuple = len(sentence), sentence[0]
+    return _tuple
