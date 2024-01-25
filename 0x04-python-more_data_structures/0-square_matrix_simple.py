5abdf5df61a5f279d9ef7687541f51aa23df961c
raim-io <raim.tobi@gmail.com>
2024-01-25 01:17:40 +0100

    0-square_matrix_simple.py: created

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    _matrix = []
+    
+    for row in matrix:
+        res = list(map(lambda x: x**2, row))
+        _matrix.append(res)
+
+    return matrix
