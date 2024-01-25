1aa4c7f281f70c941d18ba4dfbd0c6bce96b7121
raim-io <raim.tobi@gmail.com>
2024-01-25 01:46:52 +0100

    3-common_elements.py: created

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """returns a set of common elements in two sets.
+    @set_1: set to be compared
+    @set_2: set to be compared
+    """
+    comm_elem = []
+
+    for elem in set_1:
+        if elem in set_2:
+            comm_elem.append(elem)
+
+    return comm_elem
