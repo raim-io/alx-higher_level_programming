0833181fc0b5563b25e8dcafe7236f5f2cd8e29d
raim-io <raim.tobi@gmail.com>
2024-01-25 01:32:11 +0100

    1-search_replace.py: created

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
 
+def search_replace(my_list, search, replace):
+	"""replaces all occurrences of an element by
+	another in a new list
+	@my_list: initial list
+	@search: element to replace in the list
+	@replace: the new element
+	"""
+
+	_my_list = []
+
+	for item in my_list:	
+		if item == search:
+			_my_list.append(replace)
+		else:
+			_my_list.append(item)
+
+	return _my_list
