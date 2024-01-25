daa1f52d579bcd21e2e005348e4ccf97b998071b
raim-io <raim.tobi@gmail.com>
2024-01-25 02:29:17 +0100

    9-multiply_by_2.py:  resolved indentation errror

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-	"""returns a new dictionary with all values multiplied by 2"""
+    """returns a new dictionary with all values multiplied by 2"""
 
-	new_dictionary = {}
+    new_dictionary = {}
 
-	for key, value in a_dictionary.items():
-		new_dictionary.update({key: value * 2})
+    for key, value in a_dictionary.items():
+        new_dictionary.update({key: value * 2})
 
-	return new_dictionary
-	
+    return new_dictionary
+    
