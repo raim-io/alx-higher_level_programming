664b36f0d21b0df859629bf5d70fae07973251e4
raim-io <raim.tobi@gmail.com>
2024-01-25 02:21:56 +0100

    8-simple_delete.py: created"

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-	"""deletes a key in a dictionary."""
+    """deletes a key in a dictionary."""
 
-	del a_dictionary[key]
+    if key in a_dictionary:
+        del a_dictionary[key]
 
-	return a_dictionary
+    return a_dictionary
