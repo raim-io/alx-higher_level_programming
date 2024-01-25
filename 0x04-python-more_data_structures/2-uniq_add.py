e343649bf835302d47bb56602b04fd18977f1270
raim-io <raim.tobi@gmail.com>
2024-01-25 01:39:38 +0100

    2-uniq_add.py: created

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-	
+	"""adds all unique integers in a list
+	(only once for each integer).
+	"""
+
+	_my_list = []
+
+	for num in my_list:
+		if num not in _my_list:
+			sum += num
+			_my_list.append(num)
+
+	return sum
