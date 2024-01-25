44428d666457e0552e5d27759eb2ce937b86291a
raim-io <raim.tobi@gmail.com>
2024-01-25 01:52:34 +0100

    3-common_elements.py: updated comments

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -15,4 +15,4 @@ def common_elements(set_1, set_2):
 
 #alternatively,
 #def common_elements(set_1, set_2):
-#    return(set_1 & set_2)
+    return(set_1 & set_2)
---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+	"""
+	returns a set of all elements present in only one set.
+	"""
