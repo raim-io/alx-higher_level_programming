9a14586036132b5203b50880b95c4485bcda2d6a
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-06-19 15:03:27 -0700

    fix: fix 'semistandard' error

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (parseInt(process.argv[2])){
+if (parseInt(process.argv[2])) {
   console.log(`My number: ${parseInt(process.argv[2])}`);
 } else {
   console.log('Not a number');
