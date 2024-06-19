fe8ca3af43c061bd12b0c297599ac62988bdb10e
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-06-19 15:44:24 -0700

    fix: fix errors with 'square logic'

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (!parseInt(process.argv[2])) {
+if (!Number(process.argv[2])) {
   console.log('Missing size');
-} else if (parseInt(process.argv[2]) >= 0) {
-  const count = parseInt(process.argv[2]);
+} else if (Number(process.argv[2]) >= 0) {
+  const count = Number(process.argv[2]);
   let i = 0;
   while (i < count) {
     console.log('x'.repeat(count));
