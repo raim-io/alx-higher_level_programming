c3480e5c67b05e8b4f987982e5ba01bb9dd92a86
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-06-19 17:02:15 -0700

    fix: fix squares error

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   const count = Number(process.argv[2]);
   let i = 0;
   while (i < count) {
-    console.log('x'.repeat(count));
+    console.log('X'.repeat(count));
     i++;
   }
 }
