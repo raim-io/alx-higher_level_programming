10a13283ba15ce03f61fb9870143d8bab6ed8b04
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-06-20 14:56:40 -0700

    feat: Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.height = h;
+      this.width = w;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let hex = '';
+      for (let j = 0; j < this.height; j++) {
+        hex += 'X';
+      }
+
+      console.log(hex);
+    }
+  }
+}
+
+module.exports = Rectangle;
