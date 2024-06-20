a2f299699d74031af39498e94d44007a90542bf6
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-06-20 15:11:51 -0700

    feat Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,8 +2,8 @@
 class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
-      this.height = h;
       this.width = w;
+      this.height = h;
     }
   }
 
@@ -15,6 +15,17 @@ class Rectangle {
       }
       console.log(hex);
     }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
   }
 }
 
