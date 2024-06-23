db792f86474b491d8a8534dbf5f489ce9dbf3fd7
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-06-23 22:41:10 +0100

    feat: filter the states with 'a'

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""List all 'State' object"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2],
+                                   argv[3]))
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    newSession = Session()
+
+    states = newSession.query(
+        State).order_by(State.id).whereclause(
+            'states LIKE %a%')
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    newSession.close()
