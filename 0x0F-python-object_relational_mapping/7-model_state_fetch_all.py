3c4621d588f7e6c017e6304a615961f7eda8149a
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-06-23 22:22:36 +0100

    feat: Model 'State' fetch

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""List all 'State' object"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2],
+                                   argv[3]))
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    newSession = Session()
+    for state in newSession.query(State).order_by(State.id):
+        print(f"{state.id}: {state.name}")
+
+    newSession.close()
