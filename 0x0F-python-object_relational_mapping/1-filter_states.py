3f4b1149cb95db4002ec716bb1cc3a3a10ca6388
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-06-23 17:01:11 +0100

    fix: fix states filtering

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,16 +2,17 @@
 """Filter states with name starting with 'N'
 from the database 'hbtn_0e_0_usa'"""
 
-import sys
+from sys import argv
 import MySQLdb
 
 if __name__ == '__main__':
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1].startswith("N"):
+            print(row)
     cur.close()
     conn.close()
