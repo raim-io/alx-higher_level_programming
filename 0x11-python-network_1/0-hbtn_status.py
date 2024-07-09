44aa0ded50a71f15c59063cf09cfb2c091982cad
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-07-09 23:21:30 +0100

    feat: fetch url status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Fetch https://alx-intranet.hbtn.io/status"""
 
-if __name__ == '__main__':
-    import urllib
+import urllib.request
 
+if __name__ == '__main__':
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
