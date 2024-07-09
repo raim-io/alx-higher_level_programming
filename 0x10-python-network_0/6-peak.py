aa2d08cd4011b24c7e9f5f63a4733128a16d0432
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-07-09 23:13:44 +0100

    fix: fix efficiency errors

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,20 +8,18 @@ def find_peak(list_of_integers):
     if not list_of_integers:
         return None
 
-    length = len(list_of_integers)
-    mid = int(length / 2)
-    list = list_of_integers
-
-    if mid - 1 < 0 and mid + 1 >= length:
-        return list[mid]
-    elif mid - 1 < 0:
-        return list[mid] if list[mid] > list[mid + 1] else list[mid + 1]
-    elif mid + 1 >= length:
-        return list[mid] if list[mid] > list[mid - 1] else list[mid - 1]
-
-    if list[mid - 1] < list[mid] > list[mid + 1]:
-        return list[mid]
-
-    if list[mid + 1] > list[mid - 1]:
-        return find_peak(list[mid:])
-    return find_peak(list[:mid])
+    lo = 0
+    hi = len(list_of_integers)
+    mid = ((hi - lo) // 2) + lo
+    mid = int(mid)
+    if hi == 1:
+        return list_of_integers[0]
+    if hi == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
