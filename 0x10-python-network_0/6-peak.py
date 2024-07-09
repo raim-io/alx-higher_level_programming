54a6d1642b91237db4d00cfe2128aa4b47aaa025
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-07-09 23:43:01 +0100

    feat: fix pycodestyle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,7 +5,7 @@
 def find_peak(list_of_integers):
     """Finds a peak in the list of unsorted integers"""
 
-    if not list_of_integers:
+    if list_of_integers is None or list_of_integers == []:
         return None
 
     lo = 0
@@ -14,12 +14,16 @@ def find_peak(list_of_integers):
     mid = int(mid)
     if hi == 1:
         return list_of_integers[0]
+
     if hi == 2:
         return max(list_of_integers)
+
     if list_of_integers[mid] >= list_of_integers[mid - 1] and\
             list_of_integers[mid] >= list_of_integers[mid + 1]:
         return list_of_integers[mid]
+
     if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
         return find_peak(list_of_integers[mid:])
+
     if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
